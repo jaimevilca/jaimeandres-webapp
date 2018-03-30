cc658535346a82f76d3679ca92d85d7deea63bcb
Jaime Andres <jaimebusiness3@gmail.com>
2018-03-30 10:05:41 -0500

    Init Home component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 
+import { ServiceWorkerModule } from '@angular/service-worker'
+import { environment } from '../environments/environment';
+import { HomeComponent } from './home/home.component';
+import { SkillComponent } from './skill/skill.component';
+import { ProjectComponent } from './project/project.component';
+import { AboutComponent } from './about/about.component';
+import { ScrollComponent } from './scroll/scroll.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    SkillComponent,
+    ProjectComponent,
+    AboutComponent,
+    ScrollComponent
   ],
   imports: [
-    BrowserModule
-  ],
+    BrowserModule,
+    AppRoutingModule,
+    ServiceWorkerModule.register('/ngsw-worker.js', { enabled: environment.production })  ],
   providers: [],
   bootstrap: [AppComponent]
 })
